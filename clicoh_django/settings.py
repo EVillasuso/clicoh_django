d86d43aedfada8ceebe496adf18d5ec7d373407c
EVillasuso <ema.villasuso@gmail.com>
2022-03-21 09:31:56 -0300

    NS-1005-HOTFIX-Heroku_StaticFiles_Static_Root

--- clicoh_django/settings.py
+++ clicoh_django/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     'rest_framework',
     'orders',
     'django_filters',
@@ -123,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Django Rest Framework DRF Config
 REST_FRAMEWORK = {
